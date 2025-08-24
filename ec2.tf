8e19b602f5b0025b540c1722143a4aa0737b45be
vivekkoli22 <vivekkoli476@gmail.com>
2025-08-24 22:02:05 +0530

    added

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "my first instance" {
+resource "aws_instance" "my first_instance" {
     ami = "ami-00ca32bbc84273381"
     instance_type = "t2.micro"
     tags = {
-      name = "web-server"
+      Name = "web-server"
     }
 }
